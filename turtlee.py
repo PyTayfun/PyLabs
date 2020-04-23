13bf6c49b6ffa4c3888a58dca0f1547e26699ed4
Tayfun TEKŞEN <40913491+PyTayfun@users.noreply.github.com>
2020-04-23 19:30:09 +0300

    turtle.py
    Geometrik Şekil Çizme

---
+++ turtlee.py
@@ -0,0 +1,41 @@
+import turtle 
+"""
+painter = turtle.Turtle()
+
+painter.pencolor("gray")
+
+painter.speed(20)
+
+for i in range(50):
+    painter.forward(50)
+    painter.left(123) # Let's go counterclockwise this time 
+    
+painter.pencolor("blue")
+for i in range(50):
+    painter.forward(100)
+    painter.left(123)
+    
+turtle.done()
+
+"""
+import turtle 
+
+ruzgar = turtle.Turtle()
+
+ruzgar.speed(14)
+
+for i in range(36):
+    ruzgar.forward(00)
+    ruzgar.right(69)
+    ruzgar.forward(9)
+    ruzgar.left(23)
+    ruzgar.forward(60)
+    ruzgar.right(15)
+    ruzgar.pencolor("blue")
+   # ninja.penup()
+    ruzgar.setposition(0, 0)
+   # ninja.pendown()
+    
+    ruzgar.right(10)
+    
+turtle.done()
